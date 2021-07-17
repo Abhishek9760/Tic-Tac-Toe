6bd47fb446d10a7ce00443e7f0807f22b9d3aad0
Abhishek Verma <av4programming@gmail.com>
2021-07-17 10:46:58 +0530

    added sample gif

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const socketio = require('socket.io')
 const PORT = process.env.PORT || 3000
 const expressServer = app.listen(PORT)
 
-const io = socketio(expressServer, { cors: { origin: '*' } })
+const io = socketio(expressServer, { cors: { origin: 'https://jolly-northcutt-39458a.netlify.app' } })
 io.on('connection', (socket) => {
     let roomName
     console.log('connected')
